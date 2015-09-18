07a793c55bdbe5ddef639f44b7a48396b4fd939c
Eirik Vullum <evullum@gmail.com>
2015-09-18 17:14:05 +0200

    fix bug

--- example/template.js
+++ example/template.js
@@ -1,41 +1,42 @@
 exports = module.exports = {
-    active: true,
-    name: 'Dagbladet',
-    url: 'http://www.dagbladet.no/',
-    charset: 'iso-8859-1',
-    format: 'desktop',
-    template: {
-        containers: [{
-            selector: 'div.ref div.container',
-            elements: [{
-                name: 'url',
-                type: 'url',
-                occurence: 'first',
-                required: true,
-                items: [{
-                    selector: 'h2 a',
-                    attribute: 'href'
-                }]
-            }, {
-                name: 'title',
-                required: true,
-                occurence: 'first',
-                items: [{
-                    selector: 'h2 a'
-                }]
-            }, {
-                name: 'image',
-                type: 'url',
-                occurence: 'first',
-                fallback: null,
-                items: [{
-                    selector: 'img.boxImg',
-                    attribute: 'src'
-                }, {
-                    selector: 'img.lazy',
-                    attribute: 'src'
-                }]
-            }]
+  "active": true,
+  "name": "auraavis",
+  "displayName": "Aura Avis",
+  "url": "http://www.auraavis.no/",
+  "format": "desktop",
+  "template": {
+    "containers": [{
+      "selector": "article",
+      "elements": [{
+        "name": "url",
+        "type": "url",
+        "occurence": "first",
+        "required": true,
+        "items": [{
+          "selector": "a",
+          "attribute": "href"
         }]
-    }
-};
+      }, {
+        "name": "title",
+        "required": true,
+        "occurence": "all",
+        "items": [{
+          "selector": "h1"
+        }, {
+          "selector": "h2"
+        }, {
+          "selector": "h3"
+        }]
+      }, {
+        "name": "image",
+        "type": "url",
+        "occurence": "first",
+        "fallback": null,
+        "items": [{
+          "selector": "a img",
+          "attribute": "src"
+        }]
+      }]
+    }]
+  }
+}
